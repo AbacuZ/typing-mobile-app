9fdfbbcac659bde6b6787417630ca83ef0fbd231
Kitsada_Napaporn <kitsada.napaporn@accenture.com>
2021-03-08 16:20:23 +0700

    Update: inital project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private screenOrientation: ScreenOrientation) {
+    this.initialApp();
+  }
+
+  initialApp() {
+    console.log(this.screenOrientation.type);
+    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private keyboard: Keyboard) { }
+
+  onEventChange(event: any) {
+    console.log(event.target.value);
+  }
+
+  keyboardShow() {
+    this.keyboard.hide();
+  }
 
 }
